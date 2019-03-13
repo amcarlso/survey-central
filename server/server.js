0a1abf1d2c922d7b1d3727da9a91518718a6a575
Allen Carlson <allen.carlson123@gmail.com>
2019-03-13 10:21:11 -0600

    server running

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
 const express = require('express');
+require('dotenv').config();
+const {CONNECTION_STRING, SERVER_PORT} = process.env;
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening on port ${SERVER_PORT}`)
+});
