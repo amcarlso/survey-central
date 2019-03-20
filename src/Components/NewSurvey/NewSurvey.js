0efcea801b11a93986a973e3f1e8dc6d046493c2
Allen Carlson <allen.carlson123@gmail.com>
2019-03-20 16:48:29 -0600

    structured questions object to handle questionname

--- src/Components/NewSurvey/NewSurvey.js
+++ src/Components/NewSurvey/NewSurvey.js
@@ -11,7 +11,8 @@ export default class NewSurvey extends Component {
       questionName: '',
       questionNameChosen: false,
       option: '',
-      questions: {}
+      questions: {},
+      questionNumber: 1
     }
   }
 
@@ -19,54 +20,67 @@ export default class NewSurvey extends Component {
     this.setState({[prop]: e.target.value})
   }
 
-  handleLock = () => this.setState({questionNameChosen: !this.state.questionNameChosen})
-  handleUnlock = () => {
+  handleLock = () => {
     this.setState({questionNameChosen: !this.state.questionNameChosen})
     this.setState({questionName: ''})
   }
 
   handleSave = (prop) => {
-    this.setState({[prop]: !this.state[prop]})
+    this.setState({[prop]: !this.state[prop]});
+  }
+
+  handleAddQName = () => {
+    const { surveyName, surveyNameChosen, questionName, questionNameChosen, option, questions, questionNumber } = this.state;
+    questions[questionNumber] = {name: questionName};
+    this.setState({questionNumber: this.state.questionNumber + 1});
+    console.log(questions)
+    console.log(this.state.questionNumber)
   }
 
   render() {
+    const { surveyName, surveyNameChosen, questionName, questionNameChosen, option } = this.state;
     console.log(this.state)
     return (
       <div className='new-survey-page'>
         New Survey
         <div className='survey-input-container'>
           <div>
-            <span>Survey Name:</span>
-            <input 
-              type='text' 
-              placeholder='Survey Name' 
-              onChange={(e) => this.handleInput('surveyName', e)}
-            />
-            <span onClick={() => this.handleSave('surveyNameChosen')} className='save-button'>Save</span>
+              <span>Survey Name:</span>
+              <input 
+                type='text'
+                value={surveyName} 
+                onChange={(e) => this.handleInput('surveyName', e)}
+              />
+              <span 
+                onClick={() => this.handleSave('surveyNameChosen')} 
+                className='save-button'>{!surveyNameChosen ? 'Save' : 'Saved'}
+              </span>
           </div>
           <div>
-            <span>Question:</span>
-            <input 
-              disabled={!this.state.surveyNameChosen}
-              value={this.state.questionName} 
-              type='text' 
-              placeholder='Question' 
-              onChange={(e) => this.handleInput('questionName', e)}
-            />
-            {this.state.questionNameChosen ? 
-              <span onClick={() => this.handleUnlock()} className='save-button'>Locked</span> : 
-              <span onClick={() => this.handleLock()} className='save-button'>Unlocked</span>
-            }
+              <span>Question:</span>
+              <input 
+                disabled={!surveyNameChosen}
+                value={questionName} 
+                type='text' 
+                onChange={(e) => this.handleInput('questionName', e)}
+              />
+              <span 
+                onClick={() =>  {
+                  this.handleAddQName();
+                  this.handleLock();
+                }} 
+                className='save-button'>{questionNameChosen ? 'Locked' : 'Unlocked'}
+              </span>
           </div>
           <div>
-            <span>Option:</span>
-            <input 
-              disabled={!this.state.questionNameChosen}
-              type='text' 
-              placeholder='Option' 
-              onChange={(e) => this.handleInput('option', e)}
-            />
-            <span className='save-button'>Save</span>
+              <span>Option:</span>
+              <input 
+                disabled={!questionNameChosen}
+                value={option}
+                type='text' 
+                onChange={(e) => this.handleInput('option', e)}
+              />
+              <span className='save-button'>Save</span>
           </div>
         </div>
         
