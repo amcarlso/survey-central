dc4b739202a793e314dbc9069d95aa6259a40b99
Allen Carlson <allen.carlson123@gmail.com>
2019-03-20 15:51:14 -0600

    conditional rendering for disabling inputs

--- src/Components/NewSurvey/NewSurvey.js
+++ src/Components/NewSurvey/NewSurvey.js
@@ -25,8 +25,11 @@ export default class NewSurvey extends Component {
     this.setState({questionName: ''})
   }
 
+  handleSave = (prop) => {
+    this.setState({[prop]: !this.state[prop]})
+  }
+
   render() {
-    const surveyItems = this.state.questions.map
     console.log(this.state)
     return (
       <div className='new-survey-page'>
@@ -34,12 +37,22 @@ export default class NewSurvey extends Component {
         <div className='survey-input-container'>
           <div>
             <span>Survey Name:</span>
-            <input type='text' placeholder='Survey Name' onChange={(e) => this.handleInput('surveyName', e)}/>
-            <span className='save-button'>Save</span>
+            <input 
+              type='text' 
+              placeholder='Survey Name' 
+              onChange={(e) => this.handleInput('surveyName', e)}
+            />
+            <span onClick={() => this.handleSave('surveyNameChosen')} className='save-button'>Save</span>
           </div>
           <div>
             <span>Question:</span>
-            <input value={this.state.questionName} type='text' placeholder='Question' onChange={(e) => this.handleInput('questionName', e)}/>
+            <input 
+              disabled={!this.state.surveyNameChosen}
+              value={this.state.questionName} 
+              type='text' 
+              placeholder='Question' 
+              onChange={(e) => this.handleInput('questionName', e)}
+            />
             {this.state.questionNameChosen ? 
               <span onClick={() => this.handleUnlock()} className='save-button'>Locked</span> : 
               <span onClick={() => this.handleLock()} className='save-button'>Unlocked</span>
@@ -47,11 +60,16 @@ export default class NewSurvey extends Component {
           </div>
           <div>
             <span>Option:</span>
-            <input type='text' placeholder='Option' onChange={(e) => this.handleInput('option', e)}/>
+            <input 
+              disabled={!this.state.questionNameChosen}
+              type='text' 
+              placeholder='Option' 
+              onChange={(e) => this.handleInput('option', e)}
+            />
             <span className='save-button'>Save</span>
           </div>
         </div>
-        {surveyItems}
+        
       </div>
     )
   }
